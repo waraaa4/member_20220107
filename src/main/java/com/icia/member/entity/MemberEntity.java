c7f5fb930f8257525dee564ac809a9b17c6e12cb
waraaa4 <warcjh00@naver.com>
2022-01-11 22:01:50 +0900

    Initial commit, 회원가입, 로그인, 목록, 조회 완료

---
+++ src/main/java/com/icia/member/entity/MemberEntity.java
@@ -0,0 +1,38 @@
+package com.icia.member.entity;
+
+import com.icia.member.dto.MemberSaveDTO;
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.*;
+
+@Entity
+@Getter
+@Setter
+@Table(name = "member_table")
+public class MemberEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "member_id")
+    private Long id;
+
+    @Column(length = 50, unique = true)
+    private String memberEmail;
+
+    @Column(length = 20)
+    private String memberPassword;
+
+    @Column
+    private String memberName;
+
+    // MemberSaveDTO -> MemberEntity 객체로 변화하기 위한 메서드
+    public static MemberEntity saveMember(MemberSaveDTO memberSaveDTO) {
+        MemberEntity memberEntity = new MemberEntity();
+//        String memberEmail = memberSaveDTO.getMemberEmail();
+//        memberEntity.setMemberEmail(memberEmail);
+        memberEntity.setMemberEmail(memberSaveDTO.getMemberEmail());
+        memberEntity.setMemberPassword(memberSaveDTO.getMemberPassword());
+        memberEntity.setMemberName(memberSaveDTO.getMemberName());
+        return memberEntity;
+    }
+}
